60af60490693ccb7ce577852552a018e5f42e02f
Guilherme Vieira Silva <guivieirasilva02@gmail.com>
2022-06-03 17:05:32 -0300

    For no EJS template

--- EJS/index.js
+++ EJS/index.js
@@ -24,7 +24,7 @@ app.set("views", path.join(__dirname, 'views'))
 app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
-    res.render('user', users)
+    res.render('user', {users:users})
 })
 
 app.listen(PORT, () => {
